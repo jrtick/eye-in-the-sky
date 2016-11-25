63e0750a45d49aeb276e17de30cd247f7d1737aa
jrtick <jrtick@andrew.cmu.edu>
2016-11-25 22:12:44 +0000

    debugged i2c

--- i2c.py
+++ i2c.py
@@ -4,6 +4,9 @@
 import smbus
 import math,time,threading
 
+bus = None
+address = None
+
 def read_byte(adr):
     return bus.read_byte_data(address, adr)
 
@@ -32,11 +35,9 @@ def get_x_rotation(x,y,z):
     return math.degrees(radians)
 
 
-def bg_proc():
-  
-
 class Gyroscope(object):
   def __init__(self):
+    global bus,address
     self.Thread = None
     self.x = 0
     self.y = 0
@@ -48,60 +49,61 @@ class Gyroscope(object):
 
     # Now wake the 6050 up as it starts in sleep mode
     bus.write_byte_data(address, power_mgmt_1, 0)
-    def bg_proc(self):
-      while True:
-        accel_xout = read_word_2c(0x3b)
-        accel_yout = read_word_2c(0x3d)
-        accel_zout = read_word_2c(0x3f)
+  
+  def bg_proc(self):
+    while True:
+      accel_xout = read_word_2c(0x3b)
+      accel_yout = read_word_2c(0x3d)
+      accel_zout = read_word_2c(0x3f)
 
-        accel_xout_scaled = accel_xout / 16384.0
-        accel_yout_scaled = accel_yout / 16384.0
-        accel_zout_scaled = accel_zout / 16384.0
+      accel_xout_scaled = accel_xout / 16384.0
+      accel_yout_scaled = accel_yout / 16384.0
+      accel_zout_scaled = accel_zout / 16384.0
 
-        self.x=get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-        self.y=get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+      self.x=get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+      self.y=get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
 
-    def listen_background():
-      self.thread = threading.Thread(target=self.bg_proc)
-      self.thread.daemon = True
-      self.thread.start()
+  def listen_background(self):
+    self.thread = threading.Thread(target=self.bg_proc)
+    self.thread.daemon = True
+    self.thread.start()
     
-    def query():
-      return (self.x,self.y)
+  def query(self):
+    return (self.x,self.y)
 
-    def listen_foreground():
-      while True:
-        os.system("clear")
-        print "gyro data"
-        print "---------"
+  def listen_foreground(self):
+    while True:
+      os.system("clear")
+      print "gyro data"
+      print "---------"
 
-        gyro_xout = read_word_2c(0x43)
-        gyro_yout = read_word_2c(0x45)
-        gyro_zout = read_word_2c(0x47)
+      gyro_xout = read_word_2c(0x43)
+      gyro_yout = read_word_2c(0x45)
+      gyro_zout = read_word_2c(0x47)
 
-        print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
-        print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
-        print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
+      print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
+      print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
+      print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
 
-        print
-        print "accelerometer data"
-        print "------------------"
+      print
+      print "accelerometer data"
+      print "------------------"
 
-        accel_xout = read_word_2c(0x3b)
-        accel_yout = read_word_2c(0x3d)
-        accel_zout = read_word_2c(0x3f)
+      accel_xout = read_word_2c(0x3b)
+      accel_yout = read_word_2c(0x3d)
+      accel_zout = read_word_2c(0x3f)
 
-        accel_xout_scaled = accel_xout / 16384.0
-        accel_yout_scaled = accel_yout / 16384.0
-        accel_zout_scaled = accel_zout / 16384.0
+      accel_xout_scaled = accel_xout / 16384.0
+      accel_yout_scaled = accel_yout / 16384.0
+      accel_zout_scaled = accel_zout / 16384.0
 
-        print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
-        print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
-        print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
+      print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
+      print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
+      print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
 
-        print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-        print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-        time.sleep(0.1)
+      print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+      print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+      time.sleep(0.1)
 
 if(__name__=="__main__"):
   print("testing...")
