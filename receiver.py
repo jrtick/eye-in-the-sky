42c8367a1284269482d4337ed221f72c1222c9fe
jrtick <jrtick@andrew.cmu.edu>
2017-01-19 16:01:01 -0500

    updated receiver to listen in bg

--- receiver.py
+++ receiver.py
@@ -1,4 +1,4 @@
-import bluetooth
+import bluetooth,threading
 import RPi.GPIO as GPIO
 
 
@@ -8,7 +8,9 @@ FRONT_LEFT_PIN = 2
 FRONT_RIGHT_PIN = 3
 BACK_LEFT_PIN = 4
 BACK_RIGHT_PIN = 17
-
+msg = ""
+stoplistening = False
+client = None
 
 #setup pins
 GPIO.setmode(GPIO.BCM)
@@ -36,11 +38,21 @@ socket.listen(1)
 (client,client_addr) = socket.accept() #wait for connection
 print("Connected")
 
+def update_command():
+  global client,msg,stoplistening
+  while True:
+    if(not stoplistening):
+      curmsg=client.recv(1024) #blocks until message found
+      msg = curmsg #updates global message that pi sees
+
+#bg thread will keep track of client's messages and keep pi updated
+thread = threading.Thread(target=update_command)
+thread.daemon = True
+thread.start()
+
 try:
   while True:
-    msg = client.recv(1024)
-    print("heard",msg)
-    if(msg.lower()=="stop"):
+    if(msg.lower()=="stop"): #fall. fall hard.
       FL.ChangeDutyCycle(0)
       FR.ChangeDutyCycle(0)
       BL.ChangeDutyCycle(0)
@@ -48,24 +60,20 @@ try:
       break
     else:
       try:
-        pwrs = eval(msg) #[msg should be "ww xx yy zz]"
+        pwrs = eval(msg) #msg should be "[ww xx yy zz]"
       except:
+        print("couldn't evalute message")
         pwrs = [0,0,0,0]
-      for i in range(4):
-        if(pwrs[i]>40):
-          pwrs[i]=40
-        if(pwrs[i]<0):
-          pwrs[i]=0
-      print(pwrs)
       FL.ChangeDutyCycle(pwrs[0])
       FR.ChangeDutyCycle(pwrs[1])
       BL.ChangeDutyCycle(pwrs[2])
       BR.ChangeDutyCycle(pwrs[3])
-except:
+except: #also will fall hard.
   print("an error occurred?")
 
 
 #cleanup
+stoplistening=True #kill thread
 client.close()
 socket.close()
 
