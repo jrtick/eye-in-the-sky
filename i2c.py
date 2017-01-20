57fb806c2a63f2b7518883c1f4c3ecf271e1421e
jrtick <jrtick@andrew.cmu.edu>
2017-01-20 00:50:53 -0500

    made i2c into a query controller

--- i2c.py
+++ i2c.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
+#THIS IS NOT OUR CODE WE GOT IT FROM learn.sparkfun.com/tutorials/raspberry-pi-spi-and-i2c-tutorial 
 
 import smbus
-import math,os,time
-
-# Power management registers
-power_mgmt_1 = 0x6b
-power_mgmt_2 = 0x6c
+import math,time,threading
 
 def read_byte(adr):
     return bus.read_byte_data(address, adr)
@@ -34,41 +31,84 @@ def get_x_rotation(x,y,z):
     radians = math.atan2(y, dist(x,z))
     return math.degrees(radians)
 
-bus = smbus.SMBus(1)
-address = 0x68# This is the address value read via the i2cdetect command
 
-# Now wake the 6050 up as it starts in sleep mode
-bus.write_byte_data(address, power_mgmt_1, 0)
+def bg_proc():
+  
 
-while True:
-  os.system("clear")
-  print "gyro data"
-  print "---------"
+class Gyroscope(object):
+  def __init__(self):
+    self.Thread = None
+    self.x = 0
+    self.y = 0
+    # Power management registers
+    power_mgmt_1 = 0x6b
+    power_mgmt_2 = 0x6c
+    bus = smbus.SMBus(1)
+    address = 0x68# This is the address value read via the i2cdetect command
 
-  gyro_xout = read_word_2c(0x43)
-  gyro_yout = read_word_2c(0x45)
-  gyro_zout = read_word_2c(0x47)
+    # Now wake the 6050 up as it starts in sleep mode
+    bus.write_byte_data(address, power_mgmt_1, 0)
+    def bg_proc(self):
+      while True:
+        accel_xout = read_word_2c(0x3b)
+        accel_yout = read_word_2c(0x3d)
+        accel_zout = read_word_2c(0x3f)
 
-  print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
-  print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
-  print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
+        accel_xout_scaled = accel_xout / 16384.0
+        accel_yout_scaled = accel_yout / 16384.0
+        accel_zout_scaled = accel_zout / 16384.0
 
-  print
-  print "accelerometer data"
-  print "------------------"
+        self.x=get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+        self.y=get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
 
-  accel_xout = read_word_2c(0x3b)
-  accel_yout = read_word_2c(0x3d)
-  accel_zout = read_word_2c(0x3f)
+    def listen_background():
+      self.thread = threading.Thread(target=self.bg_proc)
+      self.thread.daemon = True
+      self.thread.start()
+    
+    def query():
+      return (self.x,self.y)
 
-  accel_xout_scaled = accel_xout / 16384.0
-  accel_yout_scaled = accel_yout / 16384.0
-  accel_zout_scaled = accel_zout / 16384.0
+    def listen_foreground():
+      while True:
+        os.system("clear")
+        print "gyro data"
+        print "---------"
 
-  print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
-  print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
-  print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
+        gyro_xout = read_word_2c(0x43)
+        gyro_yout = read_word_2c(0x45)
+        gyro_zout = read_word_2c(0x47)
 
-  print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-  print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-  time.sleep(0.1)
+        print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
+        print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
+        print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
+
+        print
+        print "accelerometer data"
+        print "------------------"
+
+        accel_xout = read_word_2c(0x3b)
+        accel_yout = read_word_2c(0x3d)
+        accel_zout = read_word_2c(0x3f)
+
+        accel_xout_scaled = accel_xout / 16384.0
+        accel_yout_scaled = accel_yout / 16384.0
+        accel_zout_scaled = accel_zout / 16384.0
+
+        print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
+        print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
+        print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
+
+        print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+        print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+        time.sleep(0.1)
+
+if(__name__=="__main__"):
+  print("testing...")
+  time.sleep(1)
+  gman = Gyroscope()
+  gman.listen_background()
+  while True:
+    print gman.query()
+    time.sleep(1)
+
